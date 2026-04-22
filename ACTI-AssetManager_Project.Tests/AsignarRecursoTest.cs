1d843bc78cd316a648f07ac65cafe342ab479254
Marco8735513 <marco.valenzuela253313@potros.itson.edu.mx>
2026-04-22 01:05:06 -0700

    Historial Recurso y Asignar Recurso

---
+++ ACTI-AssetManager_Project.Tests/AsignarRecursoTest.cs
@@ -0,0 +1,69 @@
+﻿using ACTI_AssetManager_Project.Application.DTOs;
+using ACTI_AssetManager_Project.Application.Interfaces;
+using ACTI_AssetManager_Project.Application.Services;
+using ACTI_AssetManager_Project.Domain.Entities;
+using ACTI_AssetManager_Project.Domain.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACTI_AssetManager_Project.Tests
+{
+    public class AsignarRecursoTest
+    {
+        private readonly Mock<IAsignacionRepository> _repoMock;
+        private readonly AsignacionService _service;
+
+        public AsignarRecursoTest()
+        {
+            // Inicializamos el Mock y el Servicio
+            _repoMock = new Mock<IAsignacionRepository>();
+            _service = new AsignacionService(_repoMock.Object);
+        }
+
+        [Fact]
+        public async Task CrearAsignacion_CuandoDatosSonValidos_RetornaTrueYGuarda()
+        {
+            // ARRANGE
+            var dto = new AsignacionDto
+            {
+                IdProyecto = 1,
+                RecursosIds = new List<int> { 101 },
+                UsuariosIds = new List<string> { "User01" },
+                IdUsuarioRegistro = "Admin"
+            };
+            _repoMock.Setup(r => r.CrearAsignacionAsync(It.IsAny<Asignacion>())).ReturnsAsync(true);
+
+            // ACT
+            var resultado = await _service.CrearAsignacionAsync(dto);
+
+            // ASSERT
+            Assert.True(resultado);
+            _repoMock.Verify(r => r.CrearAsignacionAsync(It.IsAny<Asignacion>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task CrearAsignacion_CuandoNoHayRecursos_RetornaFalseYNoGuarda()
+        {
+            // ARRANGE
+            var dto = new AsignacionDto
+            {
+                RecursosIds = new List<int>() // Lista vacía para disparar el IF
+            };
+
+            // ACT
+            var resultado = await _service.CrearAsignacionAsync(dto);
+
+            // ASSERT
+            // Aquí la prueba PASA si el resultado es falso y el repo no se tocó
+            Assert.False(resultado);
+            _repoMock.Verify(r => r.CrearAsignacionAsync(It.IsAny<Asignacion>()), Times.Never);
+        }
+
+
+
+    }
+}
---
+++ ACTI-AssetManager_Project.Tests/HistorialRecurosTest.cs
@@ -0,0 +1,69 @@
+﻿using ACTI_AssetManager_Project.Application.DTOs;
+using ACTI_AssetManager_Project.Application.Services;
+using ACTI_AssetManager_Project.Domain.Entities;
+using ACTI_AssetManager_Project.Domain.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACTI_AssetManager_Project.Tests
+{
+    public class HistorialRecurosTest
+    {
+        private readonly Mock<IAsignacionRepository> _repoMock;
+        private readonly AsignacionService _service;
+
+        public HistorialRecurosTest()
+        {
+            // Inicializamos el Mock y el Servicio
+            _repoMock = new Mock<IAsignacionRepository>();
+            _service = new AsignacionService(_repoMock.Object);
+        }
+
+
+        [Fact]
+        public async Task ObtenerTodasLasAsignaciones_NoEliminadas()
+        {
+            // ARRANGE
+            // Creamos datos de prueba usando la Entidad de Dominio (lo que devuelve el Repo)
+            var asignacionesDB = new List<Asignacion>
+    {
+        new Asignacion
+        {
+            IdAsignacion = 1,
+            Eliminado = true,
+            FechaInicio = DateTime.Now.AddDays(-1)
+        },
+        new Asignacion
+        {
+            IdAsignacion = 2,
+            Eliminado = false,
+            FechaInicio = DateTime.Now
+        },new Asignacion
+        {
+            IdAsignacion = 3,
+            Eliminado = false,
+            FechaInicio = DateTime.Now
+        }
+    };
+
+            // Configuramos el Mock para que devuelva la lista de Entidades
+            _repoMock.Setup(r => r.ObtenerTodasAsync()).ReturnsAsync(asignacionesDB);
+
+            var resultado = await _service.ObtenerTodasLasAsignacionesAsync();
+
+            // ASSERT
+            Assert.NotNull(resultado);
+
+            // CAMBIO 1: Esperamos solo 1 (el activo)
+            Assert.Equal(2, resultado.Count); 
+
+            // CAMBIO 2: Verificamos que el que llegó NO esté eliminado
+            Assert.All(resultado, x => Assert.False(x.Eliminado));
+           
+        }
+    }
+}
